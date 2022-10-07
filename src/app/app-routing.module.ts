f4c1596308cf3ecdeeb367d86ee29abb8b6afc91
Javier <javmoreno766@gmail.com>
2022-10-07 10:57:00 +0200

    feat(pages folder): add some pages
    add the folder pages and some routing things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'pages/home',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'person',
+    loadChildren: () => import('./pages/person/person.module').then( m => m.PersonPageModule)
+  },
+  {
+    path: 'task',
+    loadChildren: () => import('./pages/task/task.module').then( m => m.TaskPageModule)
+  },
+  {
+    path: 'assign',
+    loadChildren: () => import('./pages/assign/assign.module').then( m => m.AssignPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
